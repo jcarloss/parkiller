9f3f6e63b65866a85611ffb280244cd2450a5d2a
Carlos <jcarlitos.santana@gmail.com>
2016-08-05 16:14:17 -0500

    Version 1.1

--- parkiller.sql
+++ parkiller.sql
@@ -1,62 +1,95 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.4.10
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 04, 2016 at 07:27 PM
--- Server version: 5.6.26
--- PHP Version: 5.5.28
+-- Servidor: localhost
+-- Tiempo de generación: 05-08-2016 a las 23:08:52
+-- Versión del servidor: 5.5.42
+-- Versión de PHP: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `realtime`
+-- Base de datos: `parkiller`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_post`
+-- Estructura de tabla para la tabla `cat_users`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_post` (
-  `postId` int(11) NOT NULL,
-  `postText` varchar(128) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+CREATE TABLE `cat_users` (
+  `id` int(1) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `icon` varchar(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_post`
+-- Volcado de datos para la tabla `cat_users`
 --
 
-INSERT INTO `tbl_post` (`postId`, `postText`) VALUES
-(1, 'saved comment');
+INSERT INTO `cat_users` (`id`, `name`, `icon`) VALUES
+(1, 'Conductor Parkiller', 'parkiller'),
+(2, 'Cliente Parkiller', 'cliente');
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Estructura de tabla para la tabla `positions`
+--
+
+CREATE TABLE `positions` (
+  `id` int(11) NOT NULL,
+  `position` varchar(150) NOT NULL,
+  `user_type` int(1) NOT NULL DEFAULT '1',
+  `status` int(1) NOT NULL DEFAULT '1',
+  `creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `positions`
+--
+
+INSERT INTO `positions` (`id`, `position`, `user_type`, `status`, `creation`) VALUES
+(24, '19.4787354,-99.1911417', 1, 1, '2016-08-05 21:04:04'),
+(25, '19.4573434,-99.1837189', 1, 1, '2016-08-05 21:07:17'),
+(26, '19.4588061,-99.205248', 1, 1, '2016-08-05 21:07:26'),
+(27, '19.4363013,-99.1985192', 1, 1, '2016-08-05 21:07:38'),
+(28, '19.4116262,-99.1939038', 2, 1, '2016-08-05 21:07:43'),
+(29, '19.4201036,-99.179138', 2, 1, '2016-08-05 21:07:49'),
+(30, '19.4694954,-99.1922307', 2, 1, '2016-08-05 21:08:00'),
+(31, '19.4363656,-99.1439199', 2, 1, '2016-08-05 21:08:06'),
+(32, '19.4377047,-99.1495031', 2, 1, '2016-08-05 21:08:16');
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `tbl_post`
+-- Indices de la tabla `cat_users`
 --
-ALTER TABLE `tbl_post`
-  ADD PRIMARY KEY (`postId`);
+ALTER TABLE `cat_users`
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indices de la tabla `positions`
+--
+ALTER TABLE `positions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `tbl_post`
+-- AUTO_INCREMENT de la tabla `cat_users`
 --
-ALTER TABLE `tbl_post`
-  MODIFY `postId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `cat_users`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `positions`
+--
+ALTER TABLE `positions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
